f9f7a8bf4717318567002bc705744ef8e48e58f7
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-17 15:41:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,17 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-
+	monopoly = {railroads: {
+							pieces: 4,
+							names: {
+								reading_railroad: {}
+								pennsylvania_railroad: {}
+								b_and_o_railroad: {}
+								shortline: {}
+							},
+							rent_in_dollars: {}
+							}
+						}
 
 end
 
